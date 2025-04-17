75aa264a53ffa55d2094e547b7e6ea3b3ad94888
Andrés Araya Saborío <142752659+Oscaar0983721@users.noreply.github.com>
2025-04-16 21:08:57 -0600

    Create Comentario.cs

---
+++ GestionPeliculas/Models/Comentario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GestionPeliculas.Models
+{
+    public class Comentario
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public string NombreUsuario { get; set; }
+        public int ContenidoId { get; set; }
+        public string TipoContenido { get; set; } // "Pelicula" o "Serie"
+        public string Texto { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public int? Calificacion { get; set; } // Opcional, puede ser null
+
+        public Comentario()
+        {
+            FechaCreacion = DateTime.Now;
+        }
+    }
+}
